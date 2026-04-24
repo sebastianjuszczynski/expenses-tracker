900949b18d28e673348f659cf933fd9af33f7743
Sebastian Juszczyński <sebastianjuszczynski@yahoo.com>
2026-04-24 12:11:24 +0200

    feat: implement expenses management with list display and item representation

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:expenses_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expenses_tracker/models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('The chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
+  }
+}
